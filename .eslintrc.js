e85b771d4307cdb4823ce14068ccbeb5bc87be2f
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-09-15 16:33:40 +0900

    Lint our self

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+// XXX: To uniform the style of an object literals, we enable `quote-props`
+/*eslint quote-props: [2, "always"] no-magic-numbers: 0 */
+
+'use strict';
+
+/**
+ *  Test ourself :)
+ */
+module.exports = {
+    'extends': [
+        './config/eslintrc_core.js',
+    ],
+
+    'env': {
+        'commonjs': true,
+    },
+
+    'root': true,
+};
