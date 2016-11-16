5d026b4b86811d70cb2542dc53f9f2685d384bcb
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-11-16 14:11:47 +0900

    Enable node/exports-style as warning

--- config/eslintrc_node.js
+++ config/eslintrc_node.js
@@ -13,6 +13,7 @@ module.exports = {
     // https://github.com/mysticatea/eslint-plugin-node
     /* eslint sort-keys: 'error' */
     'rules': {
+        'node/exports-style': [1, 'module.exports'],
         'node/no-deprecated-api': 2, // we'd like to detect the case of using deprecated apis.
         'node/no-missing-import': 2,
         'node/no-missing-require': 2,
