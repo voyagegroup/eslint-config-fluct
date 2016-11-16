6590f07cb5202b22080964370acfcba532dc9c2e
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-11-16 14:57:01 +0900

    Enable react/no-danger

--- config/eslintrc_react.js
+++ config/eslintrc_react.js
@@ -20,7 +20,7 @@ module.exports = {
     'rules': {
         'react/display-name': 0, // Auto covered by jsx transformer.
         'react/forbid-component-props': 1,
-        'react/no-danger': 0, // ban `dangerouslySetInnerHTML`. But we still use it :(
+        'react/no-danger': 2,
         'react/no-danger-with-children': 2,
         'react/no-did-mount-set-state': [1, 'disallow-in-func'],
         'react/no-did-update-set-state': [1, 'disallow-in-func'],
