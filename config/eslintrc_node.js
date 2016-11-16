d531dc77113cc2213501ebd680df2aaf1e1a23e9
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-11-16 14:09:18 +0900

    Sort eslint-plugin-node's rules

--- config/eslintrc_node.js
+++ config/eslintrc_node.js
@@ -11,13 +11,15 @@ module.exports = {
 
     // eslint-plugin-node
     // https://github.com/mysticatea/eslint-plugin-node
+    /* eslint sort-keys: 'error' */
     'rules': {
+        'node/no-deprecated-api': 2, // we'd like to detect the case of using deprecated apis.
         'node/no-missing-import': 2,
         'node/no-missing-require': 2,
         'node/no-unpublished-import': 0, // Disable until this rule follow devDependencies
         'node/no-unpublished-require': 0, // Disable until this rule follow devDependencies
         'node/no-unsupported-features': 0, // Covered by core's `no-restricted-syntax`
         'node/shebang': 2,
-        'node/no-deprecated-api': 2, // we'd like to detect the case of using deprecated apis.
-    }
+    },
+    /* eslint-disable sort-keys */
 };
