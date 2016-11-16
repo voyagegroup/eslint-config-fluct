ef35f01f00f344237ea1055634c965629fc97726
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-11-16 14:25:18 +0900

    Enable 'node/no-unpublished-require' & node/no-unpublished-import

--- config/eslintrc_node.js
+++ config/eslintrc_node.js
@@ -18,8 +18,8 @@ module.exports = {
         'node/no-missing-import': 2,
         'node/no-missing-require': 2,
         'node/no-unpublished-bin': 0, // You should enable this rules if you develop a npm package.
-        'node/no-unpublished-import': 0, // Disable until this rule follow devDependencies
-        'node/no-unpublished-require': 0, // Disable until this rule follow devDependencies
+        'node/no-unpublished-import': 2, // Specify more details in your project.
+        'node/no-unpublished-require': 2, // Specify more details in your project.
         'node/no-unsupported-features': 0, // Covered by core's `no-restricted-syntax`
         'node/process-exit-as-throw': 2, // sort with `consistent-return` rules.
         'node/shebang': 2,
