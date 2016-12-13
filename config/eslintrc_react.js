961046d322e00f3de1dc91d4b4914a5a3a23fd1d
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-12-14 02:18:26 +0900

    Sort config/eslintrc_react.js

--- config/eslintrc_react.js
+++ config/eslintrc_react.js
@@ -28,22 +28,22 @@ module.exports = {
         'react/no-children-prop': 2, // children should be nested between the opening and closing tags.
         'react/no-danger': 2,
         'react/no-danger-with-children': 2,
+        'react/no-deprecated': 2, // Detect deprected styles
         'react/no-did-mount-set-state': [1, 'disallow-in-func'],
         'react/no-did-update-set-state': [1, 'disallow-in-func'],
         'react/no-direct-mutation-state': 2,
         'react/no-find-dom-node': 2, // Disallow to use `ReactDOM.findDOMNode()`.
+        'react/no-is-mounted': 2,
         'react/no-multi-comp': 0, // Enable to define a multiple component to a single file.
+        'react/no-render-return-value': 2,
         'react/no-set-state': 1, // Recommend to use props instead of `setState()`.
+        'react/no-string-refs': 2,
+        'react/no-unescaped-entities': 2,
         'react/no-unknown-property': 2,
         'react/no-unused-prop-types': [0, { // XXX: Disable to avoid mis-detection
             'customValidators': [],
             'skipShapeProps': false,
         }],
-        'react/no-deprecated': 2, // Detect deprected styles
-        'react/no-is-mounted': 2,
-        'react/no-render-return-value': 2,
-        'react/no-string-refs': 2,
-        'react/no-unescaped-entities': 2,
         'react/prefer-es6-class': 2,
         'react/prefer-stateless-function': [1, {
             'ignorePureComponents': false, // we'll reconsider this option when we begin to use `PureComponent`.
@@ -56,11 +56,11 @@ module.exports = {
         'react/require-optimization': [0, {
             'allowDecorators': []
         }],
+        'react/require-render-return': 2,
         'react/self-closing-comp': [2, {
             'component': true,
             'html': false,
         }],
-        'react/require-render-return': 2,
 
         // We define customized rules because we thought default settings mixes with
         // component's arguments and lifecycle methods.
@@ -100,17 +100,17 @@ module.exports = {
         'react/jsx-boolean-value': [2, 'always'], // Enforce to specify html's boolean type attribute.
         'react/jsx-closing-bracket-location': 0, // It doesn’t matter.
         'react/jsx-curly-spacing': 1,
-        'react/jsx-first-prop-new-line': [1, 'never'],
+        'react/jsx-equals-spacing': [1, 'never'],
         'react/jsx-filename-extension': [2, {
             'extensions': ['.jsx']
         }],
-        'react/jsx-equals-spacing': [1, 'never'],
+        'react/jsx-first-prop-new-line': [1, 'never'],
         'react/jsx-handler-names': [2, {
             'eventHandlerPrefix': 'on', // There is no event handler which is diffrent from this rules (`onBarFoo`).
             'eventHandlerPropPrefix': 'on',
         }],
-        'react/jsx-indent-props': 0, // we cannot force alphabetical order to our old codebase, and this is not any serious problem.
         'react/jsx-indent': [1, 4], // Sort with core's `indent` rule.
+        'react/jsx-indent-props': 0, // we cannot force alphabetical order to our old codebase, and this is not any serious problem.
         'react/jsx-key': 1,
         'react/jsx-max-props-per-line': 0, // we don't think this is serious problem.
         'react/jsx-no-bind': [2, { // Sort to bind with this in constructor.
